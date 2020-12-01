cbe86c21623512d45d08ddf380c6752b2f71ff7a
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-12-01 14:14:48 +0100

    React Navigation Setup

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Home from './screens/home';
-
+import Navigator from './routes/homeStack';
 export default function App() {
-  return <Home />;
+  return <Navigator />;
 }
