0f2c246d5904bb6f350609e1811f741037f6d9cd
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-12-04 19:44:16 +0100

    Drawer Navigation

--- App.js
+++ App.js
@@ -4,3 +4,27 @@ import Navigator from './routes/drawer';
 export default function App() {
   return <Navigator />;
 }
+/**
+  * const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+});
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Navigator />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
+
+}
+  */
