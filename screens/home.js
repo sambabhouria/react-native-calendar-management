d109793251f004e489072ed79590bdcabe21fe70
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-12-08 15:09:08 +0100

    formik submit values

--- screens/home.js
+++ screens/home.js
@@ -7,6 +7,8 @@ import {
   TouchableOpacity,
   FlatList,
   Modal,
+  TouchableWithoutFeedback,
+  Keyboard
 } from 'react-native';
 import {globalStyles} from '../styles/global';
 import Card from '../shared/card';
@@ -30,19 +32,28 @@ export default function Home({navigation}) {
     {title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3'},
   ]);
 
+  const addReview = (review) => {
+    review.key = Math.random().toString();
+    setReviews((currentReviews) => {
+      return [review, ...currentReviews];
+    });
+    setModalOpen(false);
+  };
+
   return (
     <View style={globalStyles.container}>
       <Modal visible={modalOpen} animationType="slide">
-
-        <View style={styles.modalContent}>
-          <Icon
-            name="close"
-            size={24}
-            style={{...styles.modalToggle, ...styles.modalClose}}
-            onPress={() => setModalOpen(false)}
-          />
-          <ReviewForm />
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContent}>
+            <Icon
+              name="close"
+              size={24}
+              style={{...styles.modalToggle, ...styles.modalClose}}
+              onPress={() => setModalOpen(false)}
+            />
+            <ReviewForm addReview={addReview} />
+          </View>
+        </TouchableWithoutFeedback>
       </Modal>
 
       <Icon
